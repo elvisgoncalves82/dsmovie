707c2a76e746f86fb5c6b6f3b1a1bb8b8b7a2c6c
elvis goncalves <elvisrgoncalves@hotmail.com>
2022-01-12 10:31:01 -0300

    rity config

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	@EmbeddedId
+	private ScorePk id = new ScorePk();
+	private Double value;
+	
+	public Score() {
+		
+	}
+
+	public Score(ScorePk id, Double value) {
+		super();
+		this.id = id;
+		this.value = value;
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePk getId() {
+		return id;
+	}
+
+	public void setId(ScorePk id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
+
+}
